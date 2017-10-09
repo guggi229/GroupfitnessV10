f12292872c457d12cdc3c73e942f5ad334b1f056
Guggisberg Stefan <guggs2@bfh.ch>
2017-10-09 15:42:56 +0200

    Sauberes Setup

--- Project2V10Web/src/com/guggisberg/stefan/test/Hello.java
+++ Project2V10Web/src/com/guggisberg/stefan/test/Hello.java
@@ -3,6 +3,9 @@ package com.guggisberg.stefan.test;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 
+import com.guggisberg.stefan.groupfitness.exceptions.UserNotFoundException;
+import com.guggisberg.stefan.groupfitness.services.UserService;
+import com.guggisberg.stefan.groupfitness.services.UserServiceRemote;
 import com.guggisberg.stefan.test.ejb.HelloBean;
 @ManagedBean
 public class Hello {
@@ -10,8 +13,11 @@ public class Hello {
 	@EJB
 	private HelloBean h;
 	
-	public String getWorld() {
-		return h.hello();
+	@EJB
+	private UserServiceRemote us;
+	
+	public String getWorld() throws UserNotFoundException {
+		return us.getUser(3L).getUserName();
 	}
 	
 	
